fd45fecf1535c30dd47b47b67520f884a3ab0694
Faria-Lorin <florin20@stuy.edu>
2020-10-27 23:25:23 -0400

    I added a method to deposit money into the bank account

--- BankAccount.java
+++ BankAccount.java
@@ -12,4 +12,15 @@ public class BankAccount{
   public String toString(){
     return ("Balance:" + balance + "\n" + "AccountID:" + accountID);
   }
+
+  public boolean deposit(double amount){
+    boolean success = true;
+    if (amount >= 0){
+      balance += amount;
+    }
+    else {
+      success = false;
+  }
+  return success;
+  }
 }
