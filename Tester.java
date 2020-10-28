0367a2c66c434939c5facf17aeb7068efd112745
Faria-Lorin <florin20@stuy.edu>
2020-10-28 01:57:57 -0400

    I added more tests for ddeposit and withdrawl since those have different conditions you need to check for

--- Tester.java
+++ Tester.java
@@ -1,20 +1,53 @@
 public class Tester{
   public static void main(String[] args){
-    BankAccount Joe = new BankAccount(172674837, "anemone");
+    BankAccount nimh = new BankAccount(172674837, "anemone");
+
+    //just looking at overall change in money/properties of an account
     System.out.println("Starting Balance:");
-    System.out.println(Joe.getBalance());
+    System.out.println(nimh.getBalance());
     System.out.println("\nAccountID:");
-    System.out.println(Joe.getAccountID());
+    System.out.println(nimh.getAccountID());
     System.out.println("\nOriginal Password:");
-    System.out.println(Joe.getPassword());
-    Joe.setPassword("snailFairy");
+    System.out.println(nimh.getPassword());
+    nimh.setPassword("snailFairy");
     System.out.println("\nNew Password:");
-    System.out.println(Joe.getPassword());
-    Joe.deposit(100000.59);
-    System.out.println("\nACCOUNT/BALANCE");
-    System.out.println(Joe.toString());
-    Joe.withdraw(7657.98);
-    System.out.println("\nACCOUNT/BALANCE");
-    System.out.println(Joe.toString());
+    System.out.println(nimh.getPassword());
+    nimh.deposit(100000.59);
+    System.out.println("\nACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    nimh.withdraw(7657.98);
+    System.out.println("\nACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+
+    /* test differnt true/false values of deposit
+    System.out.println("\n"+nimh.deposit(-100));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    System.out.println("\n"+nimh.deposit(0));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    System.out.println("\n"+nimh.deposit(106));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    System.out.println("\n"+nimh.deposit(72.817));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    */
+
+    /* test differnt true/false values of withdraw
+    System.out.println("\n"+nimh.withdraw(-17));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    System.out.println("\n"+nimh.withdraw(0));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    System.out.println("\n"+nimh.withdraw(23));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    System.out.println("\n"+nimh.withdraw(57.89));
+    System.out.println("ACCOUNT:        CURRENT BALANCE:");
+    System.out.println(nimh.toString());
+    */
+
   }
 }
